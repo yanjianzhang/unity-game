32197303c88097e4f4d9b6f862d36caa38dc7546
Yanjian Zhang <yanjian.zhang@outlook.com>
2024-02-26 00:07:52 +0100

    camera

--- Assets/Script/animatorController.cs
+++ Assets/Script/animatorController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class animatorController : MonoBehaviour
-{   
+{
     private Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +14,21 @@ public class animatorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("IsRun", true);
+        }
+        else
+        {
+            animator.SetBool("IsRun", false);
+        }
+
+        // if click left mouse button, triger TriggerAttack
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("TriggerAttack");
+        }
     }
 }
